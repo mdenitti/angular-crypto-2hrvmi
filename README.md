4d8731f0d25fe12e2d51fd78ab5da0dd45a87c79
mdenitti <massimodn77@gmail.com>
2022-01-25 18:53:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-crypto-2hrvmi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-crypto-2hrvmi)
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+
+import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  tokenFromUI: string = "0123456789123456";
+  encrypted: any = "";
+  decrypted: string;
+
+  request: string;
+  responce: string;
+  constructor() {
+    this.encryptUsingAES256();
+  }
+  encryptUsingAES256() {
+    let _key = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+    let _iv = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+    let encrypted = CryptoJS.AES.encrypt(
+      JSON.stringify(this.request), _key, {
+        keySize: 16,
+        iv: _iv,
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+      });
+    this.encrypted = encrypted.toString();
+  }
+  decryptUsingAES256() {
+    let _key = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+    let _iv = CryptoJS.enc.Utf8.parse(this.tokenFromUI);
+
+    this.decrypted = CryptoJS.AES.decrypt(
+      this.encrypted, _key, {
+        keySize: 16,
+        iv: _iv,
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+      }).toString(CryptoJS.enc.Utf8);
+  }
+}
